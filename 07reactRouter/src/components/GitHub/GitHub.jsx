f266d8f41048a15e674652b022e33048711b6940
user.email "shehryarkhann7@gmail.com <git config --global user.email "shehryarkhann7@gmail.com>
2024-06-14 05:19:57 +0500

    change

--- 07reactRouter/src/components/GitHub/GitHub.jsx
+++ 07reactRouter/src/components/GitHub/GitHub.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
-function GitHub() {
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
+    
   return (
-    <div>GitHub</div>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: 200
+   
+    </div>
   )
 }
 
-export default GitHub
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/shehryarkhan07')
+    return response.json()
+}  
--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -4,8 +4,9 @@ import { useParams } from 'react-router-dom'
 function User() {
     const {userid} = useParams();
   return (
-    <div>User: {userid}</div>
-  )
+    <div className='bg-gray-600 text-white text-3xl p-4'>User: {userid}</div>
+)
+  
 }
 
 export default User
