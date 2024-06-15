ffed225ac30c3af760f84592c80369598ce177d7
user.email "shehryarkhann7@gmail.com <git config --global user.email "shehryarkhann7@gmail.com>
2024-06-15 17:22:50 +0500

    change

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import { ThemeProvider } from './context/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in Theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
 
   return (
-    <>
-     <h1 className='p-4 bg-purple-600 text-3xl'>Chai </h1>
-    </>
+    
+      <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
+    
   )
 }
 
