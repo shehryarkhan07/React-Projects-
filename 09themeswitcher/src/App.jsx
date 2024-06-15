882ddb1ec5a68681ac6e84772e04507c427768b7
user.email "shehryarkhann7@gmail.com <git config --global user.email "shehryarkhann7@gmail.com>
2024-06-15 16:20:57 +0500

    themeswithcer

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='p-4 bg-purple-600 text-3xl'>Chai </h1>
+    </>
+  )
+}
+
+export default App
